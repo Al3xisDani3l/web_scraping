c9fb8b67748bd411347df3742d706be64ad83d47
Alexis Daniel Hernandez Gamez <alexisd1998_@hotmail.com>
2021-08-03 02:08:25 -0600

    Agregar archivos de proyecto.

---
+++ Indeed_web_scraping/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using HtmlAgilityPack;
+
+namespace Indeed_web_scraping
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string testUrl = "https://mx.indeed.com/jobs?q=software&l=Ciudad+Ju%C3%A1rez%2C+Chih.";
+
+            HtmlWeb web = new HtmlWeb();
+
+            HtmlDocument document = web.Load(testUrl);
+
+
+
+
+
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+
+    public class JobInformation
+    {
+        public string JobTitle { get; set; }
+    }
+
+}
